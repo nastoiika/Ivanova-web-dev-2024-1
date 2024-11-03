dc73845a31efdb0ca67231b3a749c7e4fd77530a
nastoika <146527663+nastoiika@users.noreply.github.com>
2024-11-03 17:39:10 +0300

    lab4

---
+++ lab4/dishes.js
@@ -0,0 +1,77 @@
+const dishes = [
+    {
+        keyword: "gazpacho",
+        name: "Гаспачо",
+        price: 195,
+        category: "soup",
+        count: "350 г",
+        image: "img/gazpacho.jpg"
+    },
+    {
+        keyword: "mushroom",
+        name: "Грибной суп",
+        price: 185,
+        category: "soup",
+        count: "330 г",
+        image: "img/mushroom_soup.jpg"
+    },
+    {
+        keyword: "norwegian",
+        name: "Норвежский суп",
+        price: 270,
+        category: "soup",
+        count: "330 г",
+        image: "img/norwegian_soup.jpg"
+    },
+    {
+        keyword: "fried_potatoes",
+        name: "Жареная картошка с грибами",
+        price: 150,
+        category: "main",
+        count: "250 г",
+        image: "img/friedpotatoeswithmushrooms1.jpg"
+    },
+    {
+        keyword: "lasagna",
+        name: "Лазанья",
+        price: 385,
+        category: "main",
+        count: "310 г",
+        image: "img/lasagna.jpg"
+    },
+    {
+        keyword: "cutlets",
+        name: "Котлеты из курицы с картофельным пюре",
+        price: 225,
+        category: "main",
+        count: "280 г",
+        image: "img/chickencutletsandmashedpotatoes.jpg"
+    },
+    {
+        keyword: "orange",
+        name: "Апельсиновый сок",
+        price: 120,
+        category: "drink",
+        count: "300 г",
+        image: "img/orangejuice.jpg"
+    },
+    {
+        keyword: "apple",
+        name: "Яблочный сок",
+        price: 90,
+        category: "drink",
+        count: "300 г",
+        image: "img/applejuice.jpg"
+    },
+    {
+        keyword: "carrot",
+        name: "Морковный сок",
+        price: 110,
+        category: "drink",
+        count: "300 г",
+        image: "img/carrotjuice.jpg"
+    }
+];
+
+export default dishes;
+
